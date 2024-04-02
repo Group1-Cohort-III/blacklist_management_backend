91c41e3e669d9296b3204f4cbd71fb19d2d6b66c
EtinRuyi <omoruyi.idowu@yahoo.com>
2024-04-03 00:55:25 +0100

    Setup-Pagination

--- BlackGuardApp/BlackGuardApp/obj/Debug/net7.0/BlackGuardApp.AssemblyInfo.cs
+++ BlackGuardApp/BlackGuardApp/obj/Debug/net7.0/BlackGuardApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlackGuardApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41b37c7aa235c085f9b21235cd69e02818fe3374")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39748b60678c74fb4a4be77854d9efcb19732e4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlackGuardApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlackGuardApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
