1a42ea230adbbdf2f75410d5d8aa384cd9b72168
IfeoluwaOkesola <99264083+IfeoluwaOkesola@users.noreply.github.com>
2024-04-04 22:27:55 +0100

    product repository

---
+++ BlackGuardApp/BlackGuardApp.Application/Interfaces/Repositories/IProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using BlackGuardApp.Application.Interfaces.Repositories;
+
+
+
+namespace BlackGuardApp.Application.Interfaces.Repositories
+{
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+        Task<List<Product>> GetAllProducts();
+        Task<Product> GetProductById(string id);
+        void AddProduct(Product product);
+        void UpdateProduct(Product product);
+        void DeleteProduct(Product product);
+        Task<List<Product>> FindProduct(Expression<Func<Product, bool>> condition);
+    }
+}
