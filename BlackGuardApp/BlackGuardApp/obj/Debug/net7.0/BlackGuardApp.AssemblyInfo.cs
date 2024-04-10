09decd698bec50247b45ead287539fb85d3b141f
IfeoluwaOkesola <99264083+IfeoluwaOkesola@users.noreply.github.com>
2024-04-10 02:22:11 +0100

    New Migration

--- BlackGuardApp/BlackGuardApp/obj/Debug/net7.0/BlackGuardApp.AssemblyInfo.cs
+++ BlackGuardApp/BlackGuardApp/obj/Debug/net7.0/BlackGuardApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlackGuardApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1905be5088776b82f9baff619949c1650b44662")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe4bff0f3944dc8a91ded06ae5f152d38de20796")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlackGuardApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlackGuardApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
