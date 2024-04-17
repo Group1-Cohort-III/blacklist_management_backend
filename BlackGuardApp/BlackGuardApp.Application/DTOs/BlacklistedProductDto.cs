74aa226c192b3041e91f85a6b2419b2b9785f3f3
Itunu Olaitan <itunuelijah@gmail.com>
2024-04-17 12:24:25 +0100

    Update BlacklistedProductDto.cs

--- BlackGuardApp/BlackGuardApp.Application/DTOs/BlacklistedProductDto.cs
+++ BlackGuardApp/BlackGuardApp.Application/DTOs/BlacklistedProductDto.cs
@@ -10,9 +10,9 @@ namespace BlackGuardApp.Application.DTOs
     {  
         public string BlacklistId { get; set; }
         public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
         public string CriteriaName { get; set; }
         public string CriteriaDescription { get; set; }
-
         public string CreatedBy { get; set; }
         public DateTime CreatedAt { get; set; }
         public string Reason { get; set; }
